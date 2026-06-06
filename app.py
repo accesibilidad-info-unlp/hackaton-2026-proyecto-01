260c5efd3e07ee1f70bce6ad06e4c15c5c60effd
Nicolas Sacco <nicolasfsacco@gmail.com>
2026-06-06 12:31:34 -0300

    esqueleto app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mapa Interactivo Info UNLP", layout="wide")
+
+pages = [
+    st.Page("pages/Inicio.py", title="Inicio", icon="🏠"),
+    st.Page("pages/Mapa_interactivo.py", title="Estado del sistema", icon="🗺️"),
+    st.Page("pages/Ficha_de_datos.py", title="Ficha de datos") # Oculta
+]
+
+# Inicializa la navegación ocultando el sidebar por defecto
+pg = st.navigation(pages, position="hidden")
+pg.run()
